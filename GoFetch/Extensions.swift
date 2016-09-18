13ebd2827050d8ae18179896ec7590308bc0577a
thiagok00 <thiagokda1@gmail.com>
2016-09-18 20:07:10 -0300

    Tutorial Terminado

---
+++ GoFetch/Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  Extensions.swift
+//  GoFetch
+//
+//  Created by Thiago De Angelis on 18/09/16.
+//  Copyright © 2016 BEPiD. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+    }
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
--- GoFetch/GameScene.swift
+++ GoFetch/GameScene.swift
@@ -7,83 +7,130 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Dog   : UInt32 = 0b1
+    static let Ball: UInt32 = 0b10
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let player = SKSpriteNode(imageNamed: "Girl")
     
     override func didMove(to view: SKView) {
+    
+        backgroundColor = SKColor.white
+        player.position = CGPoint(x: size.width * 0.5, y: player.size.height/2)
+        addChild(player)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(addDog),
+                SKAction.wait(forDuration: 1.0)
+                ])
+        ))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addDog() {
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        let dog = SKSpriteNode(imageNamed: "Dog1Left")
+        dog.size = CGSize(width: 128/2, height: 152/2)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        dog.physicsBody = SKPhysicsBody(rectangleOf: dog.size)
+        dog.physicsBody?.isDynamic = true
+        dog.physicsBody?.categoryBitMask = PhysicsCategory.Dog
+        dog.physicsBody?.contactTestBitMask = PhysicsCategory.Ball
+        dog.physicsBody?.collisionBitMask = PhysicsCategory.None
+        
+        let actualX = random(min: dog.size.width/2, max: size.width - dog.size.width/2)
+        
+        dog.position = CGPoint(x: actualX, y: size.height + dog.size.height/2)
+        addChild(dog)
+        
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        let actionMove = SKAction.move(to: CGPoint(x: actualX, y: -dog.size.height/2), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        dog.run(SKAction.sequence([actionMove, actionMoveDone]))
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        
+        let ball = SKSpriteNode(imageNamed: "TennisBall")
+        ball.position = player.position
+        ball.size = CGSize(width: 118/4, height: 102/4)
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.categoryBitMask = PhysicsCategory.Ball
+        ball.physicsBody?.contactTestBitMask = PhysicsCategory.Dog
+        ball.physicsBody?.collisionBitMask = PhysicsCategory.None
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        
+        let offset = touchLocation - ball.position
+        
+        if (offset.y < 0) { return }
+        
+        addChild(ball)
+        
+        let direction = offset.normalized()
+        
+        let shootAmount = direction * 1000
+        
+        let realDest = shootAmount + ball.position
+    
+        let actionMove = SKAction.move(to: realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        ball.run(SKAction.sequence([actionMove, actionMoveDone]))
+        
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func ballDidCollideWithDog(ball:SKSpriteNode, dog:SKSpriteNode) {
+        print("Catch")
+        ball.removeFromParent()
+        dog.removeFromParent()
+        
+        run(SKAction.playSoundFileNamed("woof.mp3", waitForCompletion: false))
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if ((firstBody.categoryBitMask & PhysicsCategory.Dog != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Ball != 0)) {
+            ballDidCollideWithDog(ball: firstBody.node as! SKSpriteNode, dog: secondBody.node as! SKSpriteNode)
+            
+        }
+        
     }
+    
 }
