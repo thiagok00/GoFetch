ebd19b86fd23f7f7e033d95eec0f4288eb171ad5
Ana Luiza Ferrer <analcferrer@gmail.com>
2016-09-19 16:11:54 -0300

    Dogs walking on HomeScene

--- GoFetch/GameViewController.swift
+++ GoFetch/GameViewController.swift
@@ -13,7 +13,7 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let scene = GameScene(size: view.bounds.size)
+        let scene = HomeScene(size: view.bounds.size)
         let skView = view as! SKView
         skView.showsFPS = true
         skView.showsNodeCount = true
---
+++ GoFetch/HomeScene.swift
@@ -0,0 +1,106 @@
+//
+//  HomeScene.swift
+//  GoFetch
+//
+//  Created by Ana Luiza Ferrer on 9/19/16.
+//  Copyright © 2016 BEPiD. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class HomeScene: SKScene {
+    
+    var nameLabel: SKLabelNode!
+    
+    override func didMove(to view: SKView) {
+        
+        let bgNode = SKSpriteNode(imageNamed: "Grass")
+        bgNode.size = self.size
+        bgNode.zPosition = -1
+        bgNode.position = CGPoint(x: bgNode.size.width/2, y: bgNode.size.height/2)
+        addChild(bgNode)
+        
+        nameLabel = SKLabelNode(fontNamed: "Chalkduster")
+        nameLabel.text = "Go Fetch!"
+        nameLabel.fontSize = 40
+        nameLabel.position = CGPoint(x: size.width/2, y: size.height*2/3)
+        nameLabel.fontColor = SKColor.black
+        nameLabel.zPosition = 10
+        self.addChild(nameLabel)
+        
+        let playLabel = SKLabelNode(fontNamed: "Chalkduster")
+        playLabel.text = "Play"
+        playLabel.fontSize = 40
+        playLabel.fontColor = SKColor.black
+        playLabel.position = CGPoint(x: size.width/2, y: size.height/8)
+        addChild(playLabel)
+        playLabel.name = "Play"
+        
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(addDog),
+                SKAction.wait(forDuration: 5.0)
+                ])
+        ))
+        
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        let node = self.atPoint(touchLocation)
+        
+        if node.name == "Play" {
+            self.startGame()
+        }
+        
+        
+    }
+    
+    func startGame() {
+        let reveal = SKTransition.fade(withDuration: 1.0)
+        let scene = GameScene(size: size)
+        self.view?.presentScene(scene, transition:reveal)
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addDog() {
+        
+        let dog = SKSpriteNode(imageNamed: "WalkLeft1.1")
+        dog.size = CGSize(width: 147/1.5, height: 107/1.5)
+        dog.zPosition = 15
+        dog.texture?.filteringMode = .nearest
+        let randomDog = Int(random(min: 1, max: 5))
+        let texture1 = SKTexture(imageNamed: "WalkLeft\(randomDog).1")
+        let texture2 = SKTexture(imageNamed: "WalkLeft\(randomDog).2")
+        let dogAnimation = SKAction.repeatForever(SKAction.animate(with: [texture1,texture2], timePerFrame: 0.3))
+        
+        dog.run(dogAnimation)
+        
+        let actualY = random(min: size.height - dog.size.height/2, max: dog.size.height/2)
+        
+        dog.position = CGPoint(x: size.width + dog.size.width/2, y: actualY)
+        addChild(dog)
+        
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        let actionMove = SKAction.move(to: CGPoint(x: -dog.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+    
+        dog.run(SKAction.sequence([actionMove, actionMoveDone]))
+        
+    }
+    
+}
